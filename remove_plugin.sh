a898d7b44528f5818f1400c7213eb635c0e026c1
Lifepillar <lifepillar@lifepillar.me>
2022-08-06 22:24:56 +0300

    Improve cross-compatibility of shell script.

--- remove_plugin.sh
+++ remove_plugin.sh
@@ -5,12 +5,12 @@ CURDIR=$(dirname -- $(realpath "$0"))
 cd $CURDIR
 
 # Remove the plugin directories from .gitignore
-sed -i '/autoload/d' .gitignore
-sed -i '/compiler/d' .gitignore
-sed -i '/ftdetect/d' .gitignore
-sed -i '/ftplugin/d' .gitignore
-sed -i '/indent/d' .gitignore
-sed -i '/syntax/d' .gitignore
+sed -i'' -e '/autoload/d' .gitignore
+sed -i'' -e '/compiler/d' .gitignore
+sed -i'' -e '/ftdetect/d' .gitignore
+sed -i'' -e '/ftplugin/d' .gitignore
+sed -i'' -e '/indent/d' .gitignore
+sed -i'' -e '/syntax/d' .gitignore
 
 # Delete the plugin structure
 rm -rf autoload compiler ftdetect ftplugin indent syntax
